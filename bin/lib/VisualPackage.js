8f4f2410533e6b10dfe96da1063032814bc6da71
itslenny <lenny@itslennysfault.com>
2016-04-25 17:22:59 -0700

    Add initial cli tools

---
+++ bin/lib/VisualPackage.js
@@ -0,0 +1,48 @@
+var fs = require('fs-extra');
+var path = require('path');
+
+var scriptPath = __dirname;
+var template = path.resolve(scriptPath + '/../../template');
+
+/**
+ * Represents an instance of a visual package based on file path
+ * @param {string} path - file path to root of visual package
+ */
+function VisualPackage(path) {
+    this.path = path;
+}
+
+/**
+ * Creates a new visual package from the template 
+ * @throws - throws io errors wrap in try/catch
+ */
+VisualPackage.prototype.create = function(force) {
+    if(this.exists()) {
+        if(force) {
+            fs.removeSync(this.path);
+        }
+        else {
+            throw new Error('This visual already exists. Use force to overwrite.');
+        }
+    }
+    fs.copySync(template, this.path);
+};
+
+/**
+ * Checks if the visual package path exists (directory)
+ * @returns {boolean}
+ */
+VisualPackage.prototype.exists = function() {
+    return fs.existsSync(this.path);
+};
+
+/**
+ * Checks if the visual package is valid
+ * For now this just checks if there is a pbiviz.json this could do more later
+ * @returns {boolean}
+ */
+VisualPackage.prototype.valid = function() {
+    return fs.existsSync(path.join(this.path, 'pbiviz.json'));
+};
+
+module.exports = VisualPackage;
---
+++ bin/pbiviz-start.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var fs = require('fs-extra');
+var program = require('commander');
+var VisualPackage = require('./lib/VisualPackage');
+
+program
+    //.option('-f, --force', 'force creation (overwrites folder if exists)')
+    .parse(process.argv);
+
+var args = program.args;
+var cwd = process.cwd();
+
+var package = new VisualPackage(cwd);
+
+if(!package.valid()) {
+    console.error('You must be in the root of a visual project to run this command.');
+    process.exit(1);
+}
+
+
+console.log('pbiviz start... coming soon.');
