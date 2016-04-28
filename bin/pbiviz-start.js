eca45edff658893898ba3a4c0ae79d7bd8ed914b
itslenny <lenny@itslennysfault.com>
2016-04-28 10:47:23 -0700

    Add server and certs

--- bin/pbiviz-start.js
+++ bin/pbiviz-start.js
@@ -2,9 +2,10 @@ var path = require('path');
 var fs = require('fs-extra');
 var program = require('commander');
 var VisualPackage = require('../lib/VisualPackage');
+var VisualServer = require('../lib/VisualServer');
 
 program
-    //.option('-f, --force', 'force creation (overwrites folder if exists)')
+    .option('-p, --port [port]', 'force creation (overwrites folder if exists)')
     .parse(process.argv);
 
 var args = program.args;
@@ -18,4 +19,22 @@ if(!package.valid()) {
 }
 
 
-console.log('pbiviz start... coming soon.', package.getConfig());
+// console.log('pbiviz start... coming soon.', package.getConfig());
+
+console.log('building visual');
+//TODO: build visual
+
+var assetPath = path.join(cwd, '.bin');
+
+if(!fs.existsSync(assetPath)) {
+    fs.mkdirSync(assetPath);
+    fs.writeFileSync(path.join(assetPath, 'visual.js'), "console.log('visual loaded');");
+    fs.writeFileSync(path.join(assetPath, 'visual.css'), "body { background: blue; }");
+}
+
+console.log('starting server')
+
+var server = new VisualServer(assetPath, program.port);
+server.start(function(){
+    console.log('Server listening on port ' + server.port + '.');
+});
