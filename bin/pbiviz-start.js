3a6ef90fe8ebefab7c9cf25ff1c2ba715c649c7b
Sachin Patney <spatney@microsoft.com>
2016-04-28 12:56:31 -0700

    adding builder

--- bin/pbiviz-start.js
+++ bin/pbiviz-start.js
@@ -3,6 +3,7 @@ var fs = require('fs-extra');
 var program = require('commander');
 var VisualPackage = require('../lib/VisualPackage');
 var VisualServer = require('../lib/VisualServer');
+var VisualBuilder = require('../lib/VisualBuilder');
 
 program
     .option('-p, --port [port]', 'force creation (overwrites folder if exists)')
@@ -22,19 +23,19 @@ if(!package.valid()) {
 // console.log('pbiviz start... coming soon.', package.getConfig());
 
 console.log('building visual');
-//TODO: build visual
+new VisualBuilder().build().then(function(){
+    var assetPath = path.join(cwd, '.bin');
 
-var assetPath = path.join(cwd, '.bin');
+    if(!fs.existsSync(assetPath)) {
+        fs.mkdirSync(assetPath);
+        fs.writeFileSync(path.join(assetPath, 'visual.js'), "console.log('visual loaded');");
+        fs.writeFileSync(path.join(assetPath, 'visual.css'), "body { background: blue; }");
+    }
 
-if(!fs.existsSync(assetPath)) {
-    fs.mkdirSync(assetPath);
-    fs.writeFileSync(path.join(assetPath, 'visual.js'), "console.log('visual loaded');");
-    fs.writeFileSync(path.join(assetPath, 'visual.css'), "body { background: blue; }");
-}
+    console.log('starting server')
 
-console.log('starting server')
-
-var server = new VisualServer(assetPath, program.port);
-server.start(function(){
-    console.log('Server listening on port ' + server.port + '.');
+    var server = new VisualServer(assetPath, program.port);
+    server.start(function(){
+        console.log('Server listening on port ' + server.port + '.');
+    });
 });
