c69f344f748da4c76ae4992ba41b37ed7f302de6
itslenny <lenny@itslennysfault.com>
2016-05-10 17:02:22 -0700

    Add pbibiz packaging support

--- bin/pbiviz-package.js
+++ bin/pbiviz-package.js
@@ -3,14 +3,33 @@
 let program = require('commander');
 let VisualPackage = require('../lib/VisualPackage');
 let ConsoleWriter = require('../lib/ConsoleWriter');
+let PbivizBuilder = require('../lib/PbivizBuilder');
+let VisualBuilder = require('../lib/VisualBuilder');
 
 program.parse(process.argv);
 
 let args = program.args;
 let cwd = process.cwd();
 
+
 VisualPackage.loadVisualPackage(cwd).then((visualPackage) => {
-    ConsoleWriter.info('pbiviz package... coming soon.');
-}).catch((e) => {
+
+    ConsoleWriter.info('Building visual...');
+
+    let builder = new VisualBuilder(visualPackage);
+    builder.build().then(() => {
+        ConsoleWriter.done('build complete');
+        ConsoleWriter.blank();
+        ConsoleWriter.info('Building visual...');
+        let packager = new PbivizBuilder(visualPackage);
+        packager.build().then(() => {
+            ConsoleWriter.done('packaging complete');
+        }).catch(e => {
+            ConsoleWriter.error('PACKAGE ERROR', e);
+        });
+    }).catch(e => {
+        ConsoleWriter.formattedErrors(e);
+    });
+}).catch(e => {
     ConsoleWriter.error('LOAD ERROR', e);
 });
