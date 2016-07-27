dd959a28696a00bb6c23d2be42efc3be390f7f1c
itslenny <lenny@itslennysfault.com>
2016-07-27 16:12:50 -0700

    fix formatting error

--- spec/jasmine-runner.js
+++ spec/jasmine-runner.js
@@ -1,9 +1,9 @@
 var Jasmine = require('jasmine');
 var SpecReporter = require('jasmine-spec-reporter');
-var noop = function() {};
+var noop = function () { };
 
 var jrunner = new Jasmine();
-jrunner.configureDefaultReporter({print: noop});    // remove default reporter logs
-jasmine.getEnv().addReporter(new SpecReporter());   // add jasmine-spec-reporter
-jrunner.loadConfigFile();                           // load jasmine.json configuration
+jrunner.configureDefaultReporter({ print: noop });
+jasmine.getEnv().addReporter(new SpecReporter());
+jrunner.loadConfigFile(); 
 jrunner.execute();
