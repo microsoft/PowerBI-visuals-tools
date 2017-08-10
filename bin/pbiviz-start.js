4fe8674be268cf4f01baf95f95fb3104991d0092
Dominik Moritz <domoritz@gmail.com>
2017-08-10 01:01:19 -0700

    Update pbiviz-start.js (#128)
    * Update pbiviz-start.js
    
    Fixes #127
    
    * Update pbiviz-start.js

--- bin/pbiviz-start.js
+++ bin/pbiviz-start.js
@@ -33,8 +33,8 @@ let VisualBuilder = require('../lib/VisualBuilder');
 let ConsoleWriter = require('../lib/ConsoleWriter');
 
 program
-    .option('-p, --port [port]', 'force creation (overwrites folder if exists)')
-    .option('-m, --mute', 'mute error sounds')
+    .option('-p, --port [port]', 'set the port listening on')
+    .option('-m, --mute', 'mute error outputs')
     .parse(process.argv);
 
 let cwd = process.cwd();
