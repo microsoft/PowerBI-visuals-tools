0de1c1155f36c3f9e58ead55a40e5c3d78ef3e93
erapade <par_adeen@hotmail.com>
2021-07-21 16:22:00 +0200

    Removed paddings in the rthml template (#325)
    Co-authored-by: Pär Adeen <par.adeen@sandvik.com>
    Co-authored-by: Nikita Grachev <v-grniki@microsoft.com>

--- templates/visuals/rhtml/script.r
+++ templates/visuals/rhtml/script.r
@@ -13,3 +13,7 @@ g = qplot(`Petal.Length`, data = iris, fill = `Species`, main = Sys.time());
 p = ggplotly(g);
 internalSaveWidget(p, 'out.html');
 ####################################################
+
+################ Reduce paddings ###################
+ReadFullFileReplaceString('out.html', 'out.html', ',"padding":[0-9]*,', ',"padding":0,')
+####################################################
