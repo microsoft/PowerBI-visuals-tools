64462205ffbf73404cf0eacc985f8c1ef69b2f55
AleksSavelev <v-asavelev@microsoft.com>
2022-11-23 12:07:03 +0100

    Updated dependencies

--- bin/pbiviz-start.js
+++ bin/pbiviz-start.js
@@ -79,7 +79,7 @@ VisualPackage.loadVisualPackage(cwd).then((visualPackage) => {
             ConsoleWriter.info('Starting server...');
             // webpack dev server serves bundle from disk instead memory
             if (program.drop) {
-                webpackConfig.devServer.onBeforeSetupMiddleware = (devServer) => {
+                webpackConfig.devServer.setupMiddlewares = (middlewares, devServer) => {
                     let setHeaders = (res) => {
                         Object.getOwnPropertyNames(webpackConfig.devServer.headers)
                             .forEach(property => res.header(property, webpackConfig.devServer.headers[property]));
@@ -100,6 +100,8 @@ VisualPackage.loadVisualPackage(cwd).then((visualPackage) => {
                             readFile(path.join(webpackConfig.devServer.static.directory, asset), res);
                         });
                     });
+
+                    return middlewares;
                 };
             }
 
