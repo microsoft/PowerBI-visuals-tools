8e799269fd70453fc2cf542de6b7d82d96a17a48
AleksSavelev <v-asavelev@microsoft.com>
2023-06-23 14:06:43 +0200

    Fixed errorMessage

--- src/Features/VisualVersion.ts
+++ src/Features/VisualVersion.ts
@@ -5,7 +5,7 @@ import { Severity, Stage, VisualFeatureType } from "./FeatureTypes.js";
 export default class VisualVersion implements BaseFeature {
     public static featureName = "Visual version"
     public static expectedVersionLength = 4;
-    public static errorMessage = `${this.featureName} should consist of ${this.expectedVersionLength} parts. Update your pbiviz.json file`;
+    public static errorMessage = `${this.featureName} should consist of ${this.expectedVersionLength} parts. Update the pbiviz.json file`;
     public static severity = Severity.Error
     public static stage = Stage.PreBuild
     public static visualFeatureType = VisualFeatureType.Default
