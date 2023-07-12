43defe157f4bc2a9c4f1863dfbe0745bfabc86a6
AleksSavelev <v-alsavelev@microsoft.com>
2023-07-12 16:15:31 +0200

    Returned deps related to fallback for webpack 5

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -75,7 +75,39 @@ const webpackConfig = {
         "powerbi-visuals-api": 'null'
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '.css']
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '.css'],
+        fallback: {
+            assert: require.resolve("assert/"),
+            buffer: require.resolve("buffer/"),
+            console: require.resolve("console-browserify/"),
+            constants: require.resolve("constants-browserify/"),
+            crypto: require.resolve("crypto-browserify/"),
+            domain: require.resolve("domain-browser/"),
+            events: require.resolve("events/"),
+            http: require.resolve("stream-http/"),
+            https: require.resolve("https-browserify/"),
+            os: require.resolve("os-browserify/"),
+            path: require.resolve("path-browserify/"),
+            punycode: require.resolve("punycode/"),
+            process: require.resolve("process/"),
+            querystring: require.resolve("querystring-es3/"),
+            stream: require.resolve("stream-browserify/"),
+            /* eslint-disable camelcase */
+            _stream_duplex: require.resolve("readable-stream/"),
+            _stream_passthrough: require.resolve("readable-stream/"),
+            _stream_readable: require.resolve("readable-stream/"),
+            _stream_transform: require.resolve("readable-stream/"),
+            _stream_writable: require.resolve("readable-stream/"),
+            string_decoder: require.resolve("string_decoder/"),
+            /* eslint-enable camelcase */
+            sys: require.resolve("util/"),
+            timers: require.resolve("timers-browserify/"),
+            tty: require.resolve("tty-browserify/"),
+            url: require.resolve("url/"),
+            util: require.resolve("util/"),
+            vm: require.resolve("vm-browserify/"),
+            zlib: require.resolve("browserify-zlib/")
+        }
     },
     output: {
         path: null,
